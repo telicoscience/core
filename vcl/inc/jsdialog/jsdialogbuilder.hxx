c64999db4356285bd90af8769290980ff7237728
Szymon Kłos <szymon.klos@collabora.com>
2020-02-25 13:03:34 +0100

    jsdialog: weld::ComboBox
    Change-Id: I672d2fd170e94e0b3e05384461983e5ae4a0ab35

--- vcl/inc/jsdialog/jsdialogbuilder.hxx
+++ vcl/inc/jsdialog/jsdialogbuilder.hxx
@@ -7,6 +7,7 @@
 #include <vcl/virdev.hxx>
 #include <vcl/builder.hxx>
 #include <salvtables.hxx>
+#include <vcl/combobox.hxx>
 
 class JSDialogSender
 {
@@ -26,6 +27,7 @@ public:
     virtual std::unique_ptr<weld::Dialog> weld_dialog(const OString& id, bool bTakeOwnership = true) override;
     virtual std::unique_ptr<weld::Label> weld_label(const OString &id, bool bTakeOwnership = false) override;
     virtual std::unique_ptr<weld::Entry> weld_entry(const OString& id, bool bTakeOwnership = false) override;
+    virtual std::unique_ptr<weld::ComboBox> weld_combo_box(const OString& id, bool bTakeOwnership = false) override;
 };
 
 class JSLabel : public SalInstanceLabel, public JSDialogSender
@@ -42,6 +44,27 @@ public:
     JSEntry(VclPtr<vcl::Window> aOwnedToplevel, ::Edit* pEntry,
             SalInstanceBuilder* pBuilder, bool bTakeOwnership);
     virtual void set_text(const OUString& rText) override;
+};
+
+class JSListBox : public SalInstanceComboBoxWithoutEdit, public JSDialogSender
+{
+public:
+    JSListBox(VclPtr<vcl::Window> aOwnedToplevel, ::ListBox* pListBox,
+                SalInstanceBuilder* pBuilder, bool bTakeOwnership);
+    virtual void insert(int pos, const OUString& rStr, const OUString* pId,
+                        const OUString* pIconName, VirtualDevice* pImageSurface) override;
+    virtual void remove(int pos) override;
+};
+
+class JSComboBox : public SalInstanceComboBoxWithEdit, public JSDialogSender
+{
+public:
+    JSComboBox(VclPtr<vcl::Window> aOwnedToplevel, ::ComboBox* pComboBox,
+                SalInstanceBuilder* pBuilder, bool bTakeOwnership);
+    virtual void insert(int pos, const OUString& rStr, const OUString* pId,
+                        const OUString* pIconName, VirtualDevice* pImageSurface) override;
+    virtual void remove(int pos) override;
+    virtual void set_entry_text(const OUString& rText) override;
 };
 
 #endif
